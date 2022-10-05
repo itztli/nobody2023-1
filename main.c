b665ec46e979f3959126aa2f5646a402509c1a94
Gilberto Dominguez <gilbertocarlos.dominguez@gmail.com>
2022-10-05 11:24:23 -0500

    initial files

--- main.c
+++ main.c
@@ -1,40 +1,88 @@
+// Author: Victor De la Luz
+// vdelaluz@enesmorelia.unam.mx
+// GNU/GPL
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int encrypt(int x, int key){
-  return x+key;
-}
+//MKS system
 
-int unencrypt(int x, int key){
-  return x-key;
-}
+int main(int argn, char **args){
 
-int main(int argn,char** args){
-  int a = 1;
-  int y;
-  char b = 'n';
-  char text[] = "VICTOR";
-  char encrypt_txt[7];
-  char original_txt[7];
-  int key = 2;
+  Integrator trapezio;
+  Model model3body;
+  Particle particle1,particle2,particle3;
+
+  float mass1,mass2,mass3;
+  float x1,x2,x3, y1,y2,y3, z1,z2,z3;
+  float vx1,vx2,vx3, vy1,vy2,vy3, vz1,vz2,vz3;
+
+  char name[128];
+  int N;
+
+  int integrator; // 0: default (trapezium)
+  float dt;
+
+  // Initial conditions
+  //integrator
+  integrator = 0; //trapezium
+
+  dt = 1.0; //sec
   
-  //y = f(b); 
-  //printf("n + 1 = %i\n",y);
-  //printf("%i\n",b);
+  // Model
+  sscanf("3 Body","%s",name);
+  N = 3;
+  
+  //particle 1
+  mass1 = 1000.0; //Kg
+  x1 = 0.0;  //m
+  y1 = 0.0;  //m
+  z1 = 0.0; //m
+  
+  vx1 = 0.0;  // m/s
+  vy1 = 0.0;  // m/s
+  vz1 = 0.0; //  m/s
 
-  printf("original= %s\n",text);
-  printf("encriptada= ");
-  for(int i=0; i < 6; i++ ){
-    encrypt_txt[i] = encrypt(text[i],key);
-    printf("%c",encrypt_txt[i]);
+  //particle 2
+  mass2 = 1000.0; //Kg
+  x2 = 10.0;  //m
+  y2 = 0.0;  //m
+  z2 = 0.0; //m
+  
+  vx2 = 0.0;  // m/s
+  vy2 = 0.0;  // m/s
+  vz2 = 0.0; //  m/s
+  
+  //particle 3
+  mass3 = 1000.0; //Kg
+  x3 = 20.0;  //m
+  y3 = 0.0;  //m
+  z3 = 0.0; //m
+  
+  vx3 = 0.0;  // m/s
+  vy3 = 0.0;  // m/s
+  vz3 = 0.0; //  m/s
+
+
+
+  
+  particle1 = new_Particle(mass1, x1, y1, z1, vx1, vy1, vz1);
+  particle2 = new_Particle(mass2, x2, y2, z2, vx2, vy2, vz2);
+  particle3 = new_Particle(mass3, x3, y3, z3, vx3, vy3, vz3);
+
+  model3body = new_Model(name, N);
+  
+  add_Particle_to_Model(&model3body,particle1);
+  add_Particle_to_Model(&model3body,particle2);
+  add_Particle_to_Model(&model3body,particle3);
+
+  trapezio = Integrator(integrator,dt);
+
+  for(int i=0; i<100;i++){
+    model3body = Integrator_solve(&trapecio, model3body);
+    print_Model(model3body);
   }
-  printf("\n");
-  printf("desencriptada= ");
-  for(int i=0; i < 6; i++ ){
-    original_txt[i] = unencrypt(encrypt_txt[i],key);
-    printf("%c",original_txt[i]);
-  }
-  printf("\n");  
-  //printf("%s\n",text);
+  
+
   return 0;
 }
